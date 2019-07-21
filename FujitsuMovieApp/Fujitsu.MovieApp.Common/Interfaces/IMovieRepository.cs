e4a2f487ea6df248b012f0d6df663018ae910eaf
Hassan Nasir <hnasir@dicentral.com>
2019-07-21 17:18:30 +0200

    added Movie domain object and movie repository interface

---
+++ FujitsuMovieApp/Fujitsu.MovieApp.Common/Interfaces/IMovieRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fujitsu.MovieApp.Common.Interfaces
+{
+    public interface IMovieRepository
+    {
+        void Create(Movie movie);
+        Movie Retrieve(int id);
+        void Update(Movie movie);
+        void Delete(int id);
+    }
+}
