1002ccb47d3200c434474f6c1067a029346e53bb
Hassan Nasir <hnasir@gmail.com>
2019-07-21 23:33:47 +0200

    added unit test for repository retrieve method

---
+++ FujitsuMovieApp/Fujitsu.MovieApp.Test/MovieRepository_Retrieve.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fujitsu.MovieApp.Repository.MangoDB;
+using Fujitsu.MovieApp.Common;
+
+namespace Fujitsu.MovieApp.Test
+{
+    [TestClass]
+    public class MovieRepository_Retrieve
+    {
+        MovieRepository repository;
+        Movie testMovie;
+        int id = 0;
+
+
+        [TestInitialize]
+        public void init()
+        {
+            repository = new MovieRepository();
+            testMovie = new Movie() { Title = "test movie" };
+            id = repository.Create(testMovie);
+        }
+
+        [TestCleanup]
+        public void cleanup()
+        {
+            repository.Delete(id);
+        }
+
+        [TestMethod]
+        public void ShouldReturnSingleMovieWithSameId()
+        {
+            Movie movie = repository.Retrieve(id);
+            Assert.AreEqual(id, movie.Id);
+        }
+    }
+}
