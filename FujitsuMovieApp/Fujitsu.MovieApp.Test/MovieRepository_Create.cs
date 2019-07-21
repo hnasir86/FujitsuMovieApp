72173b978beb7dbbe63ac26c6975ba13a61a8c42
Hassan Nasir <hnasir@gmail.com>
2019-07-21 23:09:54 +0200

    added unit test for create method of repository

---
+++ FujitsuMovieApp/Fujitsu.MovieApp.Test/MovieRepository_Create.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fujitsu.MovieApp.Repository.MangoDB;
+using Fujitsu.MovieApp.Common;
+
+namespace Fujitsu.MovieApp.Test
+{
+    [TestClass]
+    public class MovieRepository_Create
+    {
+        MovieRepository repository;
+        Movie testMovie;
+        int id = 0;
+
+
+        [TestInitialize]
+        public void init()
+        {
+            repository = new MovieRepository();
+            testMovie = new Movie() { Title = "test movie" };
+
+        }
+
+        [TestCleanup]
+        public void cleanup()
+        {
+            repository.Delete(id);
+        }
+
+        [TestMethod]
+        public void ShouldCreateNewMovieRecordAndReturnItsId()
+        {
+            id = repository.Create(testMovie);
+            
+            Assert.IsTrue(id > 0);
+        }
+    }
+}
