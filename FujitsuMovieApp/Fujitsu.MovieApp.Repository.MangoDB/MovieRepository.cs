8d0a30f0e3fbb8b2809bf340c6433a8525653b4e
Hassan Nasir <hnasir@gmail.com>
2019-07-22 02:23:15 +0200

    mangodb connection code and retreive method is in progress

--- FujitsuMovieApp/Fujitsu.MovieApp.Repository.MangoDB/MovieRepository.cs
+++ FujitsuMovieApp/Fujitsu.MovieApp.Repository.MangoDB/MovieRepository.cs
@@ -5,11 +5,22 @@ using System.Text;
 using System.Threading.Tasks;
 using Fujitsu.MovieApp.Common;
 using Fujitsu.MovieApp.Common.Interfaces;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace Fujitsu.MovieApp.Repository.MangoDB
 {
     public class MovieRepository : IMovieRepository
     {
+        private MongoClient client;
+        private IMongoDatabase db;
+
+        public MovieRepository()
+        {
+             client = new MongoClient("mongodb+srv://service:rSSCDZTIZzwPRsnEHGvY@cluster0-iv5oy.mongodb.net/movie?retryWrites=true");
+             db = client.GetDatabase("movie");
+        }
+
         public int Create(Movie movie)
         {
             throw new NotImplementedException();
@@ -22,7 +33,15 @@ namespace Fujitsu.MovieApp.Repository.MangoDB
 
         public Movie Retrieve(int id)
         {
-            throw new NotImplementedException();
+            Movie movie = null;
+            var client = new MongoClient("mongodb+srv://service:rSSCDZTIZzwPRsnEHGvY@cluster0-iv5oy.mongodb.net/movie?retryWrites=true");
+            var db = client.GetDatabase("movie");
+
+            var collList = db.ListCollections().ToList();
+            var movies = db.GetCollection<BsonDocument>("movie");
+            var resultDoc = movies.Find(new BsonDocument()).ToList();
+
+            return movie;
         }
 
         public void Update(Movie movie)
